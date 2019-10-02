08408870a1da5e0d34167a69b909345e9a86e7d4
Bobo <yellow_coffee@mail.ru>
2019-10-02 19:22:47 +0300

    begin

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+#include "lib.h"
+
+int main()
+{
+    cout << "build " << version() << endl;
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ version_test.cpp
@@ -0,0 +1,15 @@
+//#define BOOST_TEST_MODULE lib_test_module
+
+#include <boost/test/unit_test.hpp>
+
+#include "lib.h"
+
+BOOST_AUTO_TEST_SUITE( lib_test_suite )
+
+BOOST_AUTO_TEST_CASE( version_test )
+{
+    BOOST_CHECK(version() < 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
